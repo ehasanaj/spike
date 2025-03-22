abf694d475759661b5d5ef8427de3055ee824b20
Eraldo Hasanaj <eraldo.hasanaj@umwerk.com>
2025-03-22 21:54:33 +0100

    Rsitructured module import

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use dotenv::dotenv;
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenv().ok();
 
-    let response = tools::jira::services::issues_service::comment_on_issue("ROECIA-423", "m").await;
+    let response = tools::jira::services::issues_service::comment_on_issue("PROJ-122", "m").await;
     match response {
         Ok(_) => println!("Commented on issue"),
         Err(e) => eprintln!("Failed to comment on issue: {}", e),
